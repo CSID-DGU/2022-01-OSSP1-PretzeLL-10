9e1ccf8b61e7620b0f0ab180af0e8bf14eaa9c60
SungyoonRyu <rara0620@naver.com>
2022-05-09 00:28:16 +0900

    [RESC]add tmx and background image

---
+++ Resources/tmx/0x72_DungeonTilesetII_v1.4.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="0x72_DungeonTilesetII_v1.4" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
+ <image source="0x72_DungeonTilesetII_v1.4.png" width="512" height="512"/>
+</tileset>
---
+++ Resources/tmx/MetaSet.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="MetaSet" tilewidth="16" tileheight="16" tilecount="2" columns="2">
+ <image source="MetaSet.png" width="32" height="16"/>
+ <tile id="0">
+  <properties>
+   <property name="Wall" value="Yes"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="Item" value="Box"/>
+  </properties>
+ </tile>
+</tileset>
