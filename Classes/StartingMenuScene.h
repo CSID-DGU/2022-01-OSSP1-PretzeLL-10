d4cf7935e9564916f4ff6b135560209b87b29c9c
SungyoonRyu <rara0620@naver.com>
2022-05-09 00:22:58 +0900

    [FEAT]add 4 scene files

---
+++ Classes/StartingMenuScene.h
@@ -0,0 +1,21 @@
+#ifndef __STARTINGMENU_SCENE_H__
+#define __STARTINGMENU_SCENE_H__
+
+#include "cocos2d.h"
+
+class StartingMenu : public cocos2d::Scene
+{
+public:
+    static cocos2d::Scene* createScene();
+
+    virtual bool init();
+
+    // a selector callback
+    void menuCloseCallback(cocos2d::Ref* pSender);
+    void menuPlayCallback(cocos2d::Ref* pSender);
+
+    // implement the "static create()" method manually
+    CREATE_FUNC(StartingMenu);
+};
+
+#endif // __HELLOWORLD_SCENE_H__
