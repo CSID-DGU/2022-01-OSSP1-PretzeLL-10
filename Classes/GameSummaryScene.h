82d66a24f82673505d7c6d66f9bd61ad42c7d6ed
SungyoonRyu <rara0620@naver.com>
2022-05-11 22:35:31 +0900

    [FEAT]add Scene file named GameSummaryScene

---
+++ Classes/GameSummaryScene.h
@@ -0,0 +1,20 @@
+#ifndef __GAMESUMMARY_SCENE_H__
+#define __GAMESUMMARY_SCENE_H__
+
+#include "cocos2d.h"
+
+class GameSummary : public cocos2d::Scene
+{
+public:
+    static cocos2d::Scene* createScene();
+
+    virtual bool init();
+
+    // a selector callback
+    void menuGotoStartingMenuCallback(cocos2d::Ref* pSender);
+
+    // implement the "static create()" method manually
+    CREATE_FUNC(GameSummary);
+};
+
+#endif // __GAMESUMMARY_SCENE_H__
