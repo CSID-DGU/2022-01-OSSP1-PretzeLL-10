31a25bc87d8b0e5dbd844430b9c121a78e8ed9e6
SungyoonRyu <rara0620@naver.com>
2022-06-01 16:11:38 +0900

    [FEAT]add MonsterAI

---
+++ Classes/MonsterAI.h
@@ -0,0 +1,49 @@
+#ifndef __MONSTER_AI_H__
+#define __MONSTER_AI_H__
+
+#include <cocos2d.h>
+#include "DynamicObject.h"
+#include "Utility.h"
+
+enum class State
+{
+	WAIT,
+	ROAM,
+	ATTACK,
+	MOVE,
+	DIE
+};
+
+class MonsterAI : public cocos2d::Node
+{
+public:
+	State state;
+	int stateCount;
+	float distance;
+	float detectRange;
+	float attackRange;
+	cocos2d::Vec2 diffVec;
+	cocos2d::Vec2 roamVec;
+
+	virtual bool init() override;
+
+	virtual void update(float delta) override;
+
+	State getState() const;
+	void setState(State st, int count);
+
+	CREATE_FUNC(MonsterAI);
+public:
+	MonsterAI();
+	virtual ~MonsterAI();
+
+	void randRoamVec();
+	cocos2d::Vec2 getRoamVec();
+
+	void onEnter();
+	void updateState(float delta);
+	void getDistanceDiff();
+public:
+};
+
+#endif /* __MONSTER_AI__ */
