79536d8aa14bcde62aa90be0f4368d83e88c405b
ThatIsBug <minmin3194@naver.com>
2022-06-02 03:05:17 +0900

    [MODF] Add Header File in Monster.h

--- Classes/Monster.h
+++ Classes/Monster.h
@@ -3,6 +3,18 @@
 
 #include "BigDemon.h"
 #include "BigZombie.h"
+#include "Ogre.h"
 #include "Goblin.h"
+#include "TinyZombie.h"
+#include "Imp.h"
+#include "Skeleton.h"
+#include "Muddy.h"
+#include "Zombie.h"
+#include "MaskedOrc.h"
+#include "OrcWarrior.h"
+#include "OrcShaman.h"
+#include "Necromancer.h"
+#include "Wogol.h"
+#include "Chort.h"
 
 #endif /* __MONSTER_H__ */
