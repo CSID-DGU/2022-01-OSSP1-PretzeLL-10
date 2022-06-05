592cd176260b56f427023bfc0067afa393afbd30
SungyoonRyu <rara0620@naver.com>
2022-06-06 03:52:35 +0900

    [MODF]modify some front-end code

--- Classes/GameScene.cpp
+++ Classes/GameScene.cpp
@@ -52,20 +52,7 @@ bool GameScene::init()
     /////////////////////////////
     // 3. add your codes below...
 
-    auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
-    if (label == nullptr)
-    {
-        problemLoading("'fonts/Marker Felt.ttf'");
-    }
-    else
-    {
-        // position the label on the center of the screen
-        label->setPosition(Vec2(origin.x + visibleSize.width / 2,
-            origin.y + visibleSize.height - label->getContentSize().height));
-
-        // add the label as a child to this layer
-        this->addChild(label, 1);
-    }
+    
     _gamemapmanager = GameManager::getInstance();
 
 
