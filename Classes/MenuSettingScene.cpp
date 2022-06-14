d66f5909d659e906b84b46c34aa4f35ed1cb2172
ross1573 <ross1573@naver.com>
2022-06-15 06:22:56 +0900

    [MODF]reverted menusettingscene

--- Classes/MenuSettingScene.cpp
+++ Classes/MenuSettingScene.cpp
@@ -27,25 +27,21 @@ bool MenuSetting::init()
     }
 
     auto visibleSize = Director::getInstance()->getVisibleSize();
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
     /////////////////////////////
     // 2. add a menu item with "X" image, which is clicked to quit the program
     //    you may modify it.
 
     // add a "close" icon to exit the progress. it's an autorelease object
-    cocos2d::Label* label[3];
-    label[0] = cocos2d::Label::createWithTTF("resolution", "fonts/Marker Felt.ttf", 300);
-    label[1] = cocos2d::Label::createWithTTF("background music", "fonts/Marker Felt.ttf", 300);
-    label[2] = cocos2d::Label::createWithTTF("back", "fonts/Marker Felt.ttf", 300);
-    auto resolution = MenuItemLabel::create(label[0], CC_CALLBACK_1(MenuSetting::setDisplayResolution, this));
-    auto bgm = MenuItemLabel::create(label[1], CC_CALLBACK_1(MenuSetting::setBackgroundMusic, this));
-    auto backItem = MenuItemLabel::create(label[2], CC_CALLBACK_1(MenuSetting::menuGoMenuCallback, this));
+    auto smallDispItem = MenuItemFont::create("800x600", CC_CALLBACK_1(MenuSetting::setSmallDisp, this));
+    auto MiddleDispItem = MenuItemFont::create("1280x960", CC_CALLBACK_1(MenuSetting::setMiddleDisp, this));
+    auto backItem = MenuItemFont::create("BACK", CC_CALLBACK_1(MenuSetting::menuGoMenuCallback, this));
 
     // create menu, it's an autorelease object
-    auto menu = Menu::create(resolution, bgm, backItem, NULL);
+    auto menu = Menu::create(smallDispItem, MiddleDispItem, backItem, NULL);
     menu->alignItemsVertically();
-//    menu->setPosition(Vec2(visibleSize.width, visibleSize.height)/2);
-    menu->setScale(0.2f);
+    menu->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
     this->addChild(menu, 1);
     return true;
 }
@@ -64,31 +60,16 @@ void MenuSetting::menuCloseCallback(Ref* pSender)
 
 }
 
-void MenuSetting::setDisplayResolution(Ref* pSender)
+void MenuSetting::setSmallDisp(Ref* pSender)
 {
-    cocos2d::Label* label[2];
-    label[0] = cocos2d::Label::createWithTTF("800x600", "fonts/Marker Felt.ttf", 300);
-    label[1] = cocos2d::Label::createWithTTF("1280x960", "fonts/Marker Felt.ttf", 300);
-    auto smallLabel = cocos2d::MenuItemLabel::create(label[0], [&](Ref* s){
-        GLViewImpl* view = (GLViewImpl*)Director::getInstance()->getOpenGLView();
-        view->setWindowed(800, 600);
-    });
-    auto middle = cocos2d::MenuItemLabel::create(label[1], [&](Ref* s){
-        GLViewImpl* view = (GLViewImpl*)Director::getInstance()->getOpenGLView();
-        view->setWindowed(1280, 960);
-    });
-    
-    auto visibleSize = Director::getInstance()->getVisibleSize();
-    auto menu = Menu::create(smallLabel, middle, nullptr);
-    menu->alignItemsVertically();
-    menu->setPosition(Vec2(visibleSize.width/2, visibleSize.height/2));
-    menu->setScale(0.2f);
-    addChild(menu, 1);
+    GLViewImpl* view = (GLViewImpl*)Director::getInstance()->getOpenGLView();
+    view->setWindowed(800, 600);
 }
 
-void MenuSetting::setBackgroundMusic(Ref* pSender)
+void MenuSetting::setMiddleDisp(Ref* pSender)
 {
-    
+    GLViewImpl* view = (GLViewImpl*)Director::getInstance()->getOpenGLView();
+    view->setWindowed(1280, 960);
 }
 
 void MenuSetting::menuGoMenuCallback(Ref* pSender)
